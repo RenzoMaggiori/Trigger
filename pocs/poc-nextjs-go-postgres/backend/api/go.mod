68d00684f04492fc76e6c1938cf94d905ff9124e
Oriol Linan <oriol.linan@epitech.eu>
2024-09-16 14:40:24 +0200

    add `nextjs` + `go` + `postgres`

---
+++ pocs/poc-nextjs-go-postgres/backend/api/go.mod
@@ -0,0 +1,3 @@
+module trigger.com/api
+
+go 1.22.0
---
+++ pocs/poc-nextjs-go-postgres/backend/db/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS "todo" (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    due_date TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
