9b9d860532946fef90aed6291706ed07136a7b0a
Oriol Linan <oriol.linan@epitech.eu>
2024-09-20 23:25:19 +0200

    fix: web `Dockerized` using `dev` not `prod`

--- pocs/poc-nextjs-go-postgres/frontend/src/lib/env.ts
+++ pocs/poc-nextjs-go-postgres/frontend/src/lib/env.ts
@@ -1,20 +1,24 @@
 import { z } from "zod";
 
-export const withDevDefault = <T extends z.ZodTypeAny>(schema: T, val: z.infer<T>) =>
-    process.env["NODE_ENV"] !== "production" ? schema.default(val) : schema;
+export const withDevDefault = <T extends z.ZodTypeAny>(
+  schema: T,
+  val: z.infer<T>,
+) => (process.env["NODE_ENV"] !== "production" ? schema.default(val) : schema);
 
 export const envSchema = z.object({
-    NEXT_PUBLIC_API_URL: withDevDefault(z.string(), "http://localhost:8000"),
+  NEXT_PUBLIC_API_URL: withDevDefault(z.string(), "http://localhost:8000"),
 });
 
-const { success, data, error } = envSchema.safeParse(process.env);
+function getEnv() {
+  const { success, data, error } = envSchema.safeParse(process.env);
 
-if (!success) {
+  if (!success) {
     throw new Error(
-        "❌ Invalid environment variables:" +
-            JSON.stringify(error.format(), null, 4),
+      "❌ Invalid environment variables:" +
+        JSON.stringify(error.format(), null, 4),
     );
+  }
+  return data;
 }
 
-export const env = data;
-
+export const env = getEnv();
