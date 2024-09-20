ec5cd3ea75ecfb9844883cc64bcb9974b240f312
Renzo Maggiori <maggiorirenzo@gmail.com>
2024-09-20 20:59:33 +0200

    [add] frontend base with navbar

---
+++ mvp/frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Navbar } from "@/components/Navbar";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-full bg-gray-200 dark:bg-zinc-900">
+
+    </div>
+  );
+}
