953b61b2f63dd9014bd59bf2376764aca7223361
Oriol Linan <oriol.linan@epitech.eu>
2024-09-21 21:00:19 +0200

    fix: gmail `register` request

--- mvp/backend/api/src/endpoints/gmail/service.go
+++ mvp/backend/api/src/endpoints/gmail/service.go
@@ -1,7 +1,9 @@
 package gmail
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
@@ -79,17 +81,25 @@ func (m Model) Register(ctx context.Context) error {
 		return errors.New("could not retrieve access token")
 	}
 
-	req, err := http.NewRequest(http.MethodPost, "https://gmail.googleapis.com/gmail/v1/users/me/watch", nil)
+	body, err := json.Marshal(map[string]any{"labelIds": []string{"INBOX"}, "topicName": "projects/trigger-436310/topics/Trigger"})
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, "https://gmail.googleapis.com/gmail/v1/users/me/watch", bytes.NewBuffer(body))
 	if err != nil {
 		return err
 	}
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accessToken))
+	req.Header.Set("Content-Type", "application/json")
 
 	client := http.Client{}
 	res, err := client.Do(req)
 	if err != nil {
 		return err
 	}
+	defer res.Body.Close()
+
 	if res.StatusCode != http.StatusOK {
 		return fmt.Errorf("invalid response, got %s\n", res.Status)
 	}
