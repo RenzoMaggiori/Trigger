daef44807cf3276c93482d7d9b59f9851cbce0bd
Oriol Linan <oriol.linan@epitech.eu>
2024-09-21 21:02:52 +0200

    del: rm `comments`

--- mvp/backend/api/src/endpoints/gmail/service.go
+++ mvp/backend/api/src/endpoints/gmail/service.go
@@ -11,68 +11,6 @@ import (
 
 var _ Gmail = Model{}
 
-/* "watch": {
-"id": "gmail.users.watch",
-"path": "gmail/v1/users/{userId}/watch",
-"flatPath": "gmail/v1/users/{userId}/watch",
-"httpMethod": "POST",
-"parameters": {
-"userId": {
-"description": "The user's email address. The special value `me` can be used to indicate the authenticated user.",
-"default": "me",
-"location": "path",
-"required": true,
-"type": "string"
-}
-},
-"parameterOrder": [
-"userId"
-],
-"request": {
-"$ref": "WatchRequest"
-},
-"response": {
-"$ref": "WatchResponse"
-},
-"scopes": [
-"https://mail.google.com/",
-"https://www.googleapis.com/auth/gmail.metadata",
-"https://www.googleapis.com/auth/gmail.modify",
-"https://www.googleapis.com/auth/gmail.readonly"
-],
-"description": "Set up or update a push notification watch on the given user mailbox."
-}, */
-
-/* "WatchRequest": {
-"id": "WatchRequest",
-"description": "Set up or update a new push notification watch on this user's mailbox.",
-"type": "object",
-"properties": {
-"labelIds": {3 items},
-"labelFilterAction": {5 items},
-"labelFilterBehavior": {4 items},
-"topicName": {2 items}
-}
-}, */
-
-/* "WatchResponse": {
-"id": "WatchResponse",
-"description": "Push notification watch response.",
-"type": "object",
-"properties": {
-"historyId": {
-"description": "The ID of the mailbox's current history record.",
-"type": "string",
-"format": "uint64"
-},
-"expiration": {
-"description": "When Gmail will stop sending notifications for mailbox updates (epoch millis). Call `watch` again before this time to renew the watch.",
-"type": "string",
-"format": "int64"
-}
-}
-}, */
-
 var gmailAccessTokenKey string = "gmailAccessTokenKey"
 
 func (m Model) Register(ctx context.Context) error {
