2eac625d48c598ff5d25c3afbf728c510a2d719f
Renzo Maggiori <maggiorirenzo@gmail.com>
2024-09-21 13:17:30 +0200

    [add] magic ui

--- mvp/frontend/src/app/page.tsx
+++ mvp/frontend/src/app/page.tsx
@@ -1,3 +1,4 @@
+import WordFadeIn from "@/components/magicui/word-fade-in";
 import { Navbar } from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc";
@@ -6,13 +7,13 @@ export default function Home() {
   return (
     <div className="flex flex-1 w-full bg-gray-200 dark:bg-zinc-900 justify-center">
       <div className="flex flex-col items-center justify-start text-center text-black dark:text-white pt-20 gap-y-5">
-        <h1 className="text-5xl font-bold mb-4">
-          Connect and Automate Effortlessly
-        </h1>
-        <p className="text-xl font-bold max-w-2xl">
-          Trigger empowers you to connect services seamlessly. Automate tasks and enhance productivity by turning your ideas into efficient workflows.
-        </p>
-          <Button className="rounded-full border-black bg-white text-lg p-6" variant="outline"><FcGoogle className="mr-2"/> Sign in with Google</Button>
+        <div className="text-5xl font-bold mb-4">
+          <WordFadeIn words="Connect and Automate Effortlessly" as="h1" />
+        </div>
+        <div className="text-xl font-bold max-w-2xl">
+          <WordFadeIn as="p" words="Trigger empowers you to connect services seamlessly. Automate tasks and enhance productivity by turning your ideas into efficient workflows." />
+        </div>
+          <Button className="rounded-full border-black bg-white text-lg p-6 mt-5" variant="outline"><FcGoogle className="mr-2"/> Sign in with Google</Button>
       </div>
 
     </div>
