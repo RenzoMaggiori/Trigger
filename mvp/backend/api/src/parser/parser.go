e78393f0d542befc597defd2aa1e606efb029ed0
Oriol Linan <oriol.linan@epitech.eu>
2024-09-21 17:47:31 +0200

    rm: `port` from args, fix: `gmail` redirect uri

--- mvp/backend/api/src/parser/parser.go
+++ mvp/backend/api/src/parser/parser.go
@@ -6,17 +6,15 @@ import (
 )
 
 type Arguments struct {
-	Port    *int64
 	EnvPath *string
 }
 
 const Usage string = `
-Usage: -port <port> -env-path <path_to_env_file>
+Usage: -env-path <path_to_env_file>
 `
 
 func CmdArgs() (*Arguments, error) {
 	args := Arguments{}
-	args.Port = flag.Int64("port", 8000, "Api port")
 	args.EnvPath = flag.String("env-path", "", "Path to .env")
 	flag.Parse()
 
