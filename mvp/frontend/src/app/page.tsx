9dcb8b35f821d33a79a6e0dce0d6634d9e261f38
Renzo Maggiori <maggiorirenzo@gmail.com>
2024-09-22 12:37:10 +0200

    [add] cards to landing

--- mvp/frontend/src/app/page.tsx
+++ mvp/frontend/src/app/page.tsx
@@ -1,7 +1,11 @@
 import WordFadeIn from "@/components/magicui/word-fade-in";
-import { Navbar } from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
+import email_arrived from "/public/email_arrived.png"
+import email_sent from "/public/email_sent.png"
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -13,7 +17,37 @@ export default function Home() {
         <div className="text-xl font-bold max-w-2xl">
           <WordFadeIn as="p" words="Trigger empowers you to connect services seamlessly. Automate tasks and enhance productivity by turning your ideas into efficient workflows." />
         </div>
-          <Button className="rounded-full border-black bg-white text-lg p-6 mt-5" variant="outline"><FcGoogle className="mr-2"/> Sign in with Google</Button>
+        <Button className="rounded-full border-black bg-white text-lg p-6 mt-5" variant="outline" asChild>
+          <Link href="http://localhost:8000/api/auth/gmail/provider">
+            <FcGoogle className="mr-2" /> Sign in with Google
+          </Link>
+        </Button>
+        <div className="flex flex-row items-center mt-10 gap-5">
+          <Card className="max-w-[400px]">
+            <CardTitle>
+              <Image
+                src={email_arrived}
+                alt="email_arrived"
+                className="rounded-t-md"
+              />
+            </CardTitle>
+            <CardContent className="text-xl font-bold py-5">
+              Email Received: Trigger has successfully captured this email and is ready to process it as part of your automated workflow.
+            </CardContent>
+          </Card>
+          <Card className="max-w-[400px]">
+            <CardTitle>
+              <Image
+                src={email_sent}
+                alt="email_sent"
+                className="rounded-t-md"
+              />
+            </CardTitle>
+            <CardContent className="text-xl font-bold py-5">
+              New Email Sent: Trigger has successfully dispatched your email, seamlessly integrating it into your automated workflow.
+            </CardContent>
+          </Card>
+        </div>
       </div>
 
     </div>
