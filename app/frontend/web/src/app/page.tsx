4b84fdeb7ef98c2386c15e95ea108629ee750d0c
Renzo Maggiori <maggiorirenzo@gmail.com>
2024-09-24 20:46:08 +0200

    [add] technologies carousel

--- app/frontend/web/src/app/page.tsx
+++ app/frontend/web/src/app/page.tsx
@@ -1,13 +1,34 @@
+"use client"
 import WordFadeIn from "@/components/magicui/word-fade-in";
 import { Button } from "@/components/ui/button";
+import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
+import AutoScroll from "embla-carousel-auto-scroll";
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { BiLogoGmail } from "react-icons/bi";
+import { FaDiscord } from "react-icons/fa";
+import { IoLogoGithub } from "react-icons/io";
+import { FaSlack } from "react-icons/fa6";
+import { PiMicrosoftOutlookLogo } from "react-icons/pi";
 
 export default function Home() {
+  const plugin = React.useRef(
+    AutoScroll({ startDelay: 0 })
+  )
+
+  const services = [
+    { name: "Gmail", icon: <BiLogoGmail className="mr-2 hidden md:block" /> },
+    { name: "Discord", icon: <FaDiscord className="mr-2 hidden md:block" /> },
+    { name: "Github", icon: <IoLogoGithub className="mr-2 hidden md:block" /> },
+    { name: "Slack", icon: <FaSlack className="mr-2 hidden md:block" /> },
+    { name: "Outlook", icon: <PiMicrosoftOutlookLogo className="mr-2 hidden md:block" /> },
+  ]
+
   return (
     <div className="flex flex-1 w-full bg-gray-200 dark:bg-zinc-900 justify-center">
-      <div className="flex flex-col items-center justify-start text-center text-black dark:text-white pt-20 gap-y-5">
+      <div className="flex flex-col items-center justify-start text-center text-black dark:text-white pt-20 gap-y-5 w-full">
         <div className="text-5xl font-bold mb-4">
           <WordFadeIn words="Connect and Automate Effortlessly" as="h1" />
         </div>
@@ -16,7 +37,7 @@ export default function Home() {
         </div>
         <div className="max-w-md mx-auto flex flex-col md:flex-row gap-x-7">
           <Button className="w-full rounded-full text-lg py-6 px-12 mt-5 bg-orange-600 hover:bg-orange-700 text-white hover:text-white" variant="outline">
-              Start with Email
+            Start with Email
           </Button>
           <Button className="w-full rounded-full border-black bg-white text-lg p-6 mt-5" variant="outline" asChild>
             <Link href="http://localhost:8000/api/auth/gmail/provider" className="flex items-center">
@@ -24,7 +45,26 @@ export default function Home() {
             </Link>
           </Button>
         </div>
-
+        <div className="flex flex-col w-1/2 mt-10">
+          <Carousel
+            opts={{
+              loop: true
+            }}
+            plugins={[plugin.current]}
+            onMouseEnter={plugin.current.stop}
+            onMouseLeave={() => plugin.current.play(0)}
+          >
+            <CarouselContent className="flex">
+              {services.map((item, index) => (
+                <CarouselItem key={index} className="basis-1/2 md:basis-1/4">
+                  <div className="p-1">
+                    <span className="text-xl md:text-3xl font-semibold flex items-center justify-center text-muted-foreground">{item.icon}{item.name}</span>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
+        </div>
       </div>
 
     </div>
