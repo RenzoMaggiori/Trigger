031fe0492e0f0c022d3fb3c4df7728ca1c003f99
Renzo Maggiori <maggiorirenzo@gmail.com>
2024-09-25 16:45:41 +0200

    [add] `navbar` side menu for phone

--- app/frontend/web/src/components/ui/navbar.tsx
+++ app/frontend/web/src/components/ui/navbar.tsx
@@ -1,12 +1,13 @@
 "use client"
-import React, { useState } from 'react'
+import React from 'react'
 import { Button } from './button'
 import Link from 'next/link'
 import { LogoIcon } from './logoIcon'
 import { IoMenu } from "react-icons/io5";
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from './sheet'
 
 export function Navbar() {
-    const [loggedIn, setLoggedIn] = useState(false);
+    const [loggedIn, setLoggedIn] = React.useState(false);
 
     const navbarItems = [
         { name: "Home", href: "/home" },
@@ -15,8 +16,8 @@ export function Navbar() {
     ]
 
     const authButtons = [
-        {name: "Log In", href: "/auth?type=login", className: "rounded-full border-black text-lg", variant: "outline"},
-        {name: "Sign Up", href: "/auth?type=signup", className: "rounded-full bg-orange-600 hover:bg-orange-700 text-lg", variant: "default"},
+        { name: "Log In", href: "/auth?type=login", className: "rounded-full border-black text-lg", variant: "outline" },
+        { name: "Sign Up", href: "/auth?type=signup", className: "rounded-full bg-orange-600 hover:bg-orange-700 text-lg", variant: "default" },
     ]
 
     return (
@@ -25,9 +26,36 @@ export function Navbar() {
                 <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse absolute">
                     <LogoIcon className="h-12 w-[200px] dark:fill-white" />
                 </a>
-                <Button type='button' className="absolute right-2 md:hidden px-2" variant="ghost">
-                    <IoMenu className='h-7 w-7'/>
-                </Button>
+
+                <Sheet>
+                    <SheetTrigger className="absolute right-2 md:hidden px-2">
+                            <IoMenu className='h-7 w-7' />
+                    </SheetTrigger>
+                    <SheetContent className='w-full flex flex-col gap-5' side="right">
+                        <SheetHeader>
+                            <SheetTitle><LogoIcon className="w-1/2 dark:fill-white" /></SheetTitle>
+                            <SheetDescription className='text-start'>All reactions have a Trigger</SheetDescription>
+                        </SheetHeader>
+                        <div className='flex flex-col gap-5'>
+                            {navbarItems.map((item, key) => (
+                                <div key={key}>
+                                    <Button asChild variant="outline" className='flex items-center justify-center text-xl rounded-full border-black'>
+                                        <Link href={item.href}>
+                                            {item.name}
+                                        </Link>
+                                    </Button>
+                                </div>
+                            ))}
+                        </div>
+                        {authButtons.map((item, key) => (
+                            <Button key={key} className={item.className} variant={item.variant as "outline" | "default" | "link" | "destructive" | "secondary" | "ghost"} asChild>
+                                <Link href={item.href}>
+                                    {item.name}
+                                </Link>
+                            </Button>
+                        ))}
+                    </SheetContent>
+                </Sheet>
                 <div className="hidden w-full md:block md:w-auto mx-auto">
                     <div className='flex flex-row'>
                         {navbarItems.map((item, key) => (
