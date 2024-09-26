db9beed30b3f8e9c9f08351f9b08eb7d39801f62
Gonzalo <gonzalo.larroya-martinez@epitech.eu>
2024-09-27 00:13:49 +0200

    [add] Goth authentication

---
+++ app/backend/internal/google/types.go
@@ -0,0 +1,16 @@
+package google
+
+import (
+	"trigger.com/trigger/pkg/authenticator"
+)
+
+type Service interface {
+	authenticator.Authenticator
+}
+
+type Handler struct {
+	Service
+}
+
+type Model struct {
+}
