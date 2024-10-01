ecf3352e300c3e7c92fcd510017dbfa44fb24668
Renzo Maggiori <maggiorirenzo@gmail.com>
2024-10-01 16:43:49 +0200

    [add] `sidebar` buttons

--- app/frontend/web/src/app/home/page.tsx
+++ app/frontend/web/src/app/home/page.tsx
@@ -1,24 +1,21 @@
 "use client"
-import { Sidebar, SidebarBody, SidebarLink } from '@/components/ui/sidebar'
 import React from 'react'
-import Image from 'next/image'
-import { IconArrowLeft, IconBrandTabler, IconSettings, IconUserBolt } from '@tabler/icons-react'
 import { MdAddBox } from "react-icons/md";
 import { GrDocumentImage } from "react-icons/gr";
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
-import { Input } from '@/components/ui/input'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { ScrollArea } from '@/components/ui/scroll-area'
-import { FocusCards } from '@/components/ui/focus-cards'
+import { LogoIcon } from '@/components/ui/logoIcon'
+import { SiGooglegemini } from "react-icons/si";
 
 const page = () => {
-    const [open, setOpen] = React.useState(false);
     const links = [
         {
             href: "/",
-            name: "New Trigger",
-            icon: <MdAddBox className="text-neutral-700 dark:text-neutral-200 flex-shrink-0 mr-2" />,
+            name: "Create Trigger",
+            className: "bg-gradient-to-r from-blue-500 via-violet-500 to-fuchsia-500 animate-gradient text-white",
+            icon: <MdAddBox className="text-white flex-shrink-0 mr-2" />,
         },
         {
             name: "Templates",
@@ -26,18 +23,9 @@ const page = () => {
             icon: <GrDocumentImage className="text-neutral-700 dark:text-neutral-200 flex-shrink-0 mr-2" />,
         },
         {
-            name: "Settings",
-            href: "#",
-            icon: (
-                <IconSettings className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-            ),
-        },
-        {
-            label: "Logout",
-            href: "#",
-            icon: (
-                <IconArrowLeft className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-            ),
+            name: "Triggers",
+            href: "/",
+            icon: <SiGooglegemini className="text-neutral-700 dark:text-neutral-200 flex-shrink-0 mr-2" />,
         },
     ];
 
@@ -59,58 +47,66 @@ const page = () => {
         { title: "Card one", href: "/", img: "https://fakeimg.pl/300x200" },
         { title: "Card one", href: "/", img: "https://fakeimg.pl/300x200" },
     ]
-    React.useEffect(() => {
-        const handleResize = () => {
-            if (window.innerWidth >= 768) {
-                setOpen(true);
-            } else {
-                setOpen(false)
-            }
-        };
 
-        handleResize();
-        window.addEventListener('resize', handleResize);
 
-        return () => window.removeEventListener('resize', handleResize);
-    }, [setOpen]);
     return (
         <div className='flex h-screen w-full overflow-hidden'>
-            <Sidebar open={open}>
-                <SidebarBody className='bg-white'>
-                    <div className="mt-8 flex flex-col gap-2">
-                        {links.map((link, idx) => (
-                            <Button key={idx} variant="ghost" className='flex items-center justify-start text-center text-xl' asChild>
-                                <Link href={link.href}>
-                                    {link.icon}
-                                    {link.name}
-                                </Link>
-                            </Button>
-                        ))}
-                    </div>
+            <div className="flex flex-col h-full p-7">
+                <div className="p-4">
+                    <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-4">Dashboard</h2>
+                </div>
+                <div className='flex flex-col items-center justify-center gap-3'>
+                    {links.map((item, key) => (
+                        <Button key={key} className={`bg-white hover:bg-zinc-100 text-black ${item.className} w-full justify-start rounded-md`} asChild>
+                            <Link href={item.href}>
+                                {item.icon}
+                                <p className='text-xl'>{item.name}</p>
+                            </Link>
+                        </Button>
+                    ))}
+                </div>
+                <div className="mt-auto p-4">
+                    <Button variant="ghost" className="w-full rounded-md justify-start text-red-600 hover:text-red-700 hover:bg-red-100 dark:hover:bg-red-900">
+                        <p>Logout</p>
+                    </Button>
+                </div>
+            </div>
+            <div className='flex flex-col w-full p-5 overflow-x-auto'>
+                <Card className='py-6'>
+                    <CardContent>
+                        <ScrollArea>
+                        <div className='px-8 py-4 w-full'>
 
-                </SidebarBody>
-            </Sidebar>
-            <div className='flex flex-col w-full'>
-                <p className='text-3xl font-bold p-5'>Your Triggers</p>
+                            <Card className='w-full h-[200px] bg-gradient-to-tr from-blue-500 via-violet-500 to-fuchsia-500 p-5 rounded-lg shadow-lg animate-gradient'>
+                                <CardContent className='flex flex-col h-full items-center justify-center text-white text-center font-bold text-lg lg:text-3xl gap-y-3'>
+                                    <LogoIcon className='w-[150px] fill-yellow-500' />
+                                    <p>Try Trigger for 30 days free</p>
+                                    <Button className='bg-zinc-200 text-black p-5 hover:bg-zinc-100'>Start free trial</Button>
+                                </CardContent>
+                            </Card>
+                        </div>
 
-                <ScrollArea>
-                    <div className='flex flex-row flex-wrap gap-4 p-5 overflow-x-auto'>
-                        {triggers.map((trigger, index) => (
-                            <div key={index}>
-                                <Link href={trigger.href}>
-                                    <Card className='flex flex-col bg-zinc-100 shadow-md rounded-lg w-[200px]' key={index}>
-                                        <CardHeader className='p-4 border-b'>
-                                            <CardTitle className='text-xl font-bold'>
-                                                <img src={trigger.img} alt={trigger.title} />
-                                            </CardTitle>
-                                        </CardHeader>
-                                    </Card>
-                                    <p className='font-bold text-md text-start p-1'>{trigger.title}</p>
-                                </Link>
+                        <p className='text-3xl font-bold p-5'>Your Triggers</p>
+
+                            <div className='flex flex-row flex-wrap gap-4 p-5 items-center justify-center'>
+                                {triggers.concat(triggers).map((trigger, index) => (
+                                    <div key={index}>
+                                        <Link href={trigger.href}>
+                                            <Card className='flex flex-col bg-zinc-100 shadow-md rounded-lg w-[200px]' key={index}>
+                                                <CardHeader className='p-4 border-b'>
+                                                    <CardTitle className='text-xl font-bold'>
+                                                        <img src={trigger.img} alt={trigger.title} />
+                                                    </CardTitle>
+                                                </CardHeader>
+                                            </Card>
+                                            <p className='font-bold text-md text-start p-1'>{trigger.title}</p>
+                                        </Link>
+                                    </div>
+                                ))}
                             </div>
-                        ))}
-                    </div>
-                </ScrollArea>
+                        </ScrollArea>
+                    </CardContent>
+                </Card>
             </div>
         </div>
     )
