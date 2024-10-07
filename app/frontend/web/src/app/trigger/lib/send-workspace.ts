7d5e81ffa0659b772d54a09d4da682d1d4c8c21d
Renzo Maggiori <maggiorirenzo@gmail.com>
2024-10-07 12:25:38 +0200

    [fix] `access_token` missing on send-workspace

--- app/frontend/web/src/app/trigger/lib/send-workspace.ts
+++ app/frontend/web/src/app/trigger/lib/send-workspace.ts
@@ -1,12 +1,16 @@
-import React from 'react'
+"use server"
+
 import { TriggerWorkspace } from './types'
 import { env } from '@/lib/env';
+import { cookies } from 'next/headers';
 
 export async function send_workspace(triggerWorkspace: TriggerWorkspace) {
+    const access_token = cookies().get("access_token")?.value;
     const res = await fetch(`${env.NEXT_PUBLIC_AUTH_SERVICE_URL}/api/workspaces`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            'Authorization': `Bearer ${access_token}`,
         },
         body: JSON.stringify(triggerWorkspace),
     });
