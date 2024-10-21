65b23af3d7f840e442a0a97e04a59f301a52ebe7
Oriol Linan <oriol.linan@epitech.eu>
2024-10-21 17:58:37 +0200

    fix: `env` variables for callback

--- app/backend/internal/sync/routes.go
+++ app/backend/internal/sync/routes.go
@@ -3,10 +3,12 @@ package sync
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net/http"
 	"os"
 
 	// "github.com/markbates/goth/providers/github"
+	"github.com/markbates/goth/providers/github"
 	"github.com/markbates/goth/providers/google"
 	"go.mongodb.org/mongo-driver/mongo"
 	"trigger.com/trigger/pkg/mongodb"
@@ -18,7 +20,7 @@ func Router(ctx context.Context) (*router.Router, error) {
 	if !ok {
 		return nil, errors.New("could not find sync mongo collection")
 	}
-	
+
 	server := http.NewServeMux()
 
 	//* providers
@@ -36,16 +38,21 @@ func Router(ctx context.Context) (*router.Router, error) {
 		google.New(
 			os.Getenv("GOOGLE_CLIENT_ID"),
 			os.Getenv("GOOGLE_CLIENT_SECRET"),
-			"http://localhost:8005/api/oauth2/callback",
+			fmt.Sprintf("%s/api/sync/callback", os.Getenv("SYNC_SERVICE_BASE_URL")),
 			"https://mail.google.com/",
 			"https://www.googleapis.com/auth/documents",
 			"https://www.googleapis.com/auth/drive",
 			"https://www.googleapis.com/auth/userinfo.profile",
 			"email",
 		),
+		github.New(
+			os.Getenv("GITHUB_KEY"),
+			os.Getenv("GITHUB_SECRET"),
+			fmt.Sprintf("%s/api/sync/callback", os.Getenv("SYNC_SERVICE_BASE_URL")),
+		),
 	)
 
 	server.Handle("GET /sync-with", http.HandlerFunc(handler.SyncWith))
 	server.Handle("GET /callback", http.HandlerFunc(handler.Callback))
-	return router.NewRouter("/oauth2", server), nil
+	return router.NewRouter("/sync", server), nil
 }
