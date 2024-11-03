057cc14f288bd4110a652a2887ee99e7f989be4f
Renzo Maggiori <maggiorirenzo@gmail.com>
2024-11-03 17:45:08 +0100

    [fix] `build error`

--- app/frontend/web/src/app/trigger/lib/transform-custom-nodes.ts
+++ app/frontend/web/src/app/trigger/lib/transform-custom-nodes.ts
@@ -7,6 +7,7 @@ export function transformCustomNodes(customNodes: CustomNode[], edges: Edge[], e
   for (const node of customNodes) {
     nodes[node.id] = {
       id: node.id,
+      status: (existingNodes && existingNodes[node.id]) ? existingNodes[node.id]?.action_id : "inactive",
       action_id:  (existingNodes && existingNodes[node.id]) ? existingNodes[node.id]?.action_id : "default",
       fields: {},
       parent_ids: edges
